b98cf68505fe5293843f6cc9d9ae33002c7e229e
Ubuntu <datonearash@gmail.com>
2020-11-22 12:14:08 -0800

    added main functino

--- source/main.c
+++ source/main.c
@@ -20,36 +20,39 @@
 int outputA = 0x00;
 
 
-enum STATES{keyCases}state;
+enum STATES{Start,keyCases}state;
 
 int tick(int state){
 	unsigned char y;
 	y = GetKeypadKey();
 	switch(state){
+		case Start:
+			state = keyCases;
+			break;
 		case keyCases:
 		switch(y) {
-			case '\0': outputA = 0x1F; break;
-			case '1': outputA = 0x01; break;
-			case '2': outputA = 0x02; break;
-			case '3': outputA = 0x03; break;
-                        case '4': outputA = 0x04; break;
-                        case '5': outputA = 0x05; break;
-                        case '6': outputA = 0x06; break;
-                        case '7': outputA = 0x07; break;
-                        case '8': outputA = 0x08; break;
-                        case '9': outputA = 0x09; break;
-                        case 'A': outputA = 0x0A; break;
-                        case 'B': outputA = 0x0B; break;
-                        case 'C': outputA = 0x0C; break;
-                        case 'D': outputA = 0x0D; break;
-                        case '*': outputA = 0x0E; break;
-                        case '0': outputA = 0x00; break;
-                        case '#': outputA = 0x0F; break;
-                        default: outputA = 0x1B; break;
-		}
-		state = keyCases;
-		PORTB = outputA;
-		break;
+				case '\0': outputA = 0x1F; break;
+				case '1': outputA = 0x01; break;
+				case '2': outputA = 0x02; break;
+				case '3': outputA = 0x03; break;
+                        	case '4': outputA = 0x04; break;
+                        	case '5': outputA = 0x05; break;
+                        	case '6': outputA = 0x06; break;
+                        	case '7': outputA = 0x07; break;
+                        	case '8': outputA = 0x08; break;
+                        	case '9': outputA = 0x09; break;
+                        	case 'A': outputA = 0x0A; break;
+                        	case 'B': outputA = 0x0B; break;
+                        	case 'C': outputA = 0x0C; break;
+                        	case 'D': outputA = 0x0D; break;
+                        	case '*': outputA = 0x0E; break;
+                        	case '0': outputA = 0x00; break;
+                        	case '#': outputA = 0x0F; break;
+                        	default: outputA = 0x1B; break;
+				}	
+			state = keyCases;
+			PORTB = outputA;
+			break;
 	}
 	return state;
 }
@@ -58,10 +61,36 @@ int tick(int state){
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRC = 0xF0; PORTC = 0x0F;	
     /* Insert your solution below */
-    while (1) {
+	static task task1;
+	task *tasks[] = { &task1 };
+	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 
-    }
-    return 1;
+	unsigned char GCD = 10; //temp value
+
+	const char start = -1;
+
+	task1.state = start;
+	task1.period = 100;
+	task1.elapsedTime = task1.period;
+	task1.TickFct = &tick;
+
+	TimerSet(GCD);
+	TimerOn();
+
+	unsigned short i;
+    	while (1) {
+		for(i = 0; i < numTasks; i++){
+			if(tasks[i]->elapsedTime == tasks[i]->period){
+				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+				tasks[i]->elapsedTime = 0;
+			}
+			tasks[i]->elapsedTime += GCD;
+		}
+		while(!TimerFlag);
+		TimerFlag = 0;
+    	}
+    return 0;
 }
